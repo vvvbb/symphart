c3461bfab3b30ca80f6c08cd290c2718ee863864
vincent <vb@vvbb.fr>
2020-10-12 18:38:22 +0200

    Create, Update & Delete

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+// alert(1);
+const articles = document.getElementById("articles");
+
+if (articles) {
+  articles.addEventListener("click", (e) => {
+    if (e.target.className === "btn btn-danger delete-article") {
+      //   alert(3);
+      if (confirm("Are you sure ?")) {
+        const id = e.target.getAttribute("data-id");
+
+        // alert(id);
+        fetch(`/article/delete/${id}`, {
+          method: "DELETE",
+        }).then((res) => window.location.reload());
+      }
+    }
+  });
+}
